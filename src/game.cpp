d635ac03b402fa3ac806bab319c913046b0a8653
thekian <thekian@ryuu>
2010-02-11 20:46:57 +0100

    Kewl rect.. triangle

--- src/game.cpp
+++ src/game.cpp
@@ -1,8 +1,29 @@
+#include <SDL/SDL.h>
+#include <SDL/SDL_opengl.h>
+
 #include "game.h"
 
 int game(void)
 {
-	while(true) {
+	bool quit = false;
+	SDL_Event event;
+	
+	while(!quit) {
+		if( SDL_PollEvent( &event ) ) {
+			if( event.type == SDL_QUIT ) {
+				quit=true;
+			}
+		}
+		
+		glColor3f( 1.0, 1.0, 1.0);
+		glBegin( GL_QUADS );
+			glVertex3f( 0.0, 0.0, -10.0);
+			glVertex3f( 0.0, 25.0,-10.0);
+			glVertex3f(25.0, 25.0,-10.0);
+			glVertex3f(15.0, 25.0,-10.0);
+		glEnd();
+		
+		SDL_GL_SwapBuffers();
 	}
 	
 	return 0;
--- src/init.cpp
+++ src/init.cpp
@@ -1,5 +1,6 @@
 #include <SDL/SDL.h>
 #include <SDL/SDL_opengl.h>
+#include <GL/gl.h>
 
 #include "init.h"
 
@@ -7,6 +8,16 @@ int init(void)
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
 	
-	SDL_Surface *draw;
-	draw = SDL_SetVideoMode(1024,768, 0, SDL_OPENGL);
+	SDL_SetVideoMode(1024,768, 0, SDL_OPENGL);
+	
+	SDL_WM_SetCaption( "Labyrinth Fighter /o/", NULL );
+	
+	glClearColor( 1,0,0,0);
+	
+	glMatrixMode( GL_PROJECTION );
+	glLoadIdentity();
+	//glOrtho( 0,0,1024, 768, 10, -10);
+	gluPerspective( 75.0, 1024.0/768.0, 0.1, 1000.0 );
+	
+	return 0;
 }
