d080ff821cd1118e9ec6247f5c1d62154ade7e4a
thekian <thekian@ryuu>
2010-04-08 20:49:03 +0200

    Cool stuff

--- src/player.cpp
+++ src/player.cpp
@@ -5,37 +5,27 @@ void Player::setCamera()
 {
     glMatrixMode( GL_PROJECTION );
 	glLoadIdentity();
-	gluPerspective( 75.0, 1024.0/768.0, 0.1, 1000.0 );
-	printf("%f\n%f\n%f\n\n", rot.getIndex(0,1), rot.getIndex(1,1), rot.getIndex(2,1));
+	gluPerspective( 75.0, 1024.0/768.0, 0.1, 1.0e6 );
 	
-	//rot.roll(0.0001);
-	//rot.yaw(-0.001);
-	//rot.pitch(0.002);
     gluLookAt(x,y,z,
 			x+rot.getIndex(0,0), y+rot.getIndex(1,0), z+rot.getIndex(2,0),
-			x+rot.getIndex(0,1), y+rot.getIndex(1,1), z+rot.getIndex(2,1));
+			rot.getIndex(0,1), rot.getIndex(1,1), rot.getIndex(2,1));
 }
 
 void Player::process()
 {
-	SDL_Event key;
-	if(SDL_PollEvent(&key)) {
-		switch(key.type) {
-			case SDL_PRESSED:
-				switch(key.key.keysym.sym) {
-					case SDLK_UP:
-						printf("Hello!");
-						rot.pitch(-0.001);
-						break;
-						
-					case SDLK_s:
-						rot.pitch(0.001);
-						break;
-				}
-		}
-	}
+	if(k_up)	rot.pitch(0.05);
+	if(k_down)	rot.pitch(-0.05);
+	if(k_left)	rot.roll(-0.05);
+	if(k_right) rot.roll(0.05);
+	vx += double(k_forth)*rot.getIndex(0,0)*0.05;
+	vy += double(k_forth)*rot.getIndex(1,0)*0.05;
+	vz += double(k_forth)*rot.getIndex(2,0)*0.05;
 	
-    x+=vx;
-    y+=vy;
-    z+=vz;
+	if(k_break)
+		vx=vy=vz=0.0;
+	
+	x+=vx;
+	y+=vy;
+	z+=vz;
 }
--- src/player.h
+++ src/player.h
@@ -7,6 +7,7 @@ class Player {
         Player() {
             x=y=z=0.0;
             vx=vy=vz=0.0;
+			k_up=k_down=k_left=k_right=k_forth=k_break=false;
 			rot.roll(-0.5);
         }
         
@@ -16,6 +17,7 @@ class Player {
         void setCamera();
         void process();
         
+		bool k_up, k_down, k_left, k_right, k_forth, k_break;
     private:
         double x,y,z;
         double vx,vy,vz;
