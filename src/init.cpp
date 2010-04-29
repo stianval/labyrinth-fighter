bdcaeef521f91343127f29ef04b495fd15e027aa
TZer0 <TZer0.jan@gmail.com>
2010-04-29 21:29:32 +0200

    More stuff

--- src/init.cpp
+++ src/init.cpp
@@ -1,8 +1,14 @@
+#include <cstdlib>
+#include <ctime>
 #include "port.h"
 #include "init.h"
+#include "cube.h"
+
+extern Cube maze[MAP_SIZE];
 
 int init(void)
 {
+	srand(time(NULL));
 	SDL_Init(SDL_INIT_EVERYTHING);
 	
 	SDL_SetVideoMode(1024,768, 0, SDL_OPENGL);
@@ -27,5 +33,9 @@ int init(void)
     /* Really Nice Perspective Calculations */
     glHint( GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST );
 	
+	for (int i=0; i<MAP_SIZE;i++){
+		maze[i].x = rand() & 1;
+	}
+
 	return 0;
 }
