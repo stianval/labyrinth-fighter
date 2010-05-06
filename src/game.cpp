e1e8e749fc50578802233b5b8556649a34550225
TZer0 <TZer0.jan@gmail.com>
2010-05-06 20:49:56 +0200

    More stuff

---
+++ src/game.cpp
@@ -0,0 +1,107 @@
+#include <math.h>
+#include "port.h"
+#include "game.h"
+#include "player.h"
+#include "draw.h"
+#include "maze.h"
+
+int game(void)
+{
+	double k = 0.0, l=0.0, m=0.0;
+	int prev,cur;
+	bool quit = false;
+	SDL_Event event;
+	Player pl;
+	
+	prev = SDL_GetTicks();
+	while(!quit) {
+		while( SDL_PollEvent( &event ) ) {
+			switch( event.type ) {
+				case SDL_QUIT:
+					quit=true;
+					break;
+					
+				case SDL_KEYDOWN:
+				case SDL_KEYUP:
+					{
+						bool state = event.key.state;
+						pl.procState(event.key.keysym.sym, state);
+						switch( event.key.keysym.sym ) {
+                            /*
+							case SDLK_UP: pl.k_up = state;	break;
+							case SDLK_DOWN: pl.k_down = state;	break;
+							case SDLK_LEFT: pl.k_left = state;	break;
+							case SDLK_RIGHT: pl.k_right = state;break;
+							case SDLK_w: pl.k_forth = state; break;
+                            case SDLK_b: pl.k_brakedebug = state; break;
+                            case SDLK_s: pl.k_brake = state; break;
+							case SDLK_a: pl.k_tleft = state; break;
+							case SDLK_d: pl.k_tright = state; break;
+                            */
+							/*
+                            case SDLK_UP:    set_state(&pl.keystate, K_UP, state); break;
+							case SDLK_DOWN:  set_state(&pl.keystate, K_DOWN, state); break;
+							case SDLK_LEFT:  set_state(&pl.keystate, K_LEFT, state); break;
+							case SDLK_RIGHT: set_state(&pl.keystate, K_RIGHT, state); break;
+							case SDLK_w:     set_state(&pl.keystate, K_FORTH, state); break;
+                            case SDLK_b:     set_state(&pl.keystate, K_BRAKEDEBUG, state); break;
+                            case SDLK_s:     set_state(&pl.keystate, K_BRAKE, state); break;
+							case SDLK_a:     set_state(&pl.keystate, K_TLEFT, state); break;
+							case SDLK_d:     set_state(&pl.keystate, K_TRIGHT, state); break;|
+							*/
+                            case SDLK_ESCAPE:   quit=true; break;
+						}
+					}
+					break;
+			}
+		}
+		
+		if(SDL_GetTicks() > prev+(1000/60)) {
+			prev = SDL_GetTicks();
+			
+			pl.process();
+			pl.setCamera();
+			
+            /*
+			Draw.resetCursor();
+			Draw.movePosition(0,0, -6);
+			Draw.rotate(k, 0.0,1.0,0.0);
+			Draw.rotate(l, 1.0,0.0,0.0);
+			Draw.rotate(m, 0.0,0.0,1.0);
+			// Try to center the cube for rotation
+			Draw.movePosition(-0.5,-0.5,-0.5);
+			//k+=0.1; l+=0.01; m+=0.001;
+			//Draw.render(mybox);
+			
+			Draw.movePosition(-2,+1,-4);		Draw.render(mybox);
+			Draw.movePosition(+4,-2,+8);		Draw.render(mybox);
+			
+			Draw.movePosition(-2,+1,-4);
+			Draw.scale(0.5,0.5,0.5);
+			Draw.movePosition(3,3,0);		Draw.render(mybox);
+			Draw.movePosition(-6,-6,0);		Draw.render(mybox);
+		
+			Draw.resetCursor();
+			Draw.scale(10000,10000,10000);
+
+
+            /*
+
+			Draw.resetCursor();
+			//glDisable( GL_DEPTH_TEST);
+			Draw.movePosition(-500,-500,-500);
+			Draw.scale(1000,1000,1000);		Draw.render(mybox);
+			
+            */
+			
+			maze.draw();
+
+			/* Draw it to the screen */
+			SDL_GL_SwapBuffers( );
+			glClearColor( k/1000.0,l/1000.0,m/1000.0, 0.0f );
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		}
+	}
+	
+	return 0;
+}
---
+++ src/player.cpp
@@ -0,0 +1,112 @@
+#include "port.h"
+#include "player.h"
+#include "debug.h"
+#include <cstring>
+
+#define ROT_ADJ 0.002
+#define VEL_ADJ 0.050
+
+Player::Player() {
+	x=y=z=0.0;
+	vx=vy=vz=0.0;
+	droll=dpitch=dyaw=0.0;
+	keystate = 0;
+    //k_up=k_down=k_left=k_right=k_forth=k_brake=k_tleft=k_tright=k_brakedebug=false;
+	rot.roll(-0.5);
+}
+
+void Player::setCamera()
+{
+    glMatrixMode( GL_PROJECTION );
+	glLoadIdentity();
+	gluPerspective( 75.0, 1024.0/768.0, 0.1, 1.0e6 );
+	
+    gluLookAt(x,y,z,
+			x+rot.getIndex(0,0), y+rot.getIndex(1,0), z+rot.getIndex(2,0),
+			rot.getIndex(0,1), rot.getIndex(1,1), rot.getIndex(2,1));
+}
+
+void Player::process()
+{
+    /*
+	if(k_up)	 dpitch-=0.002;
+	if(k_down)	 dpitch+=0.002;
+	if(k_left)	 droll-=0.002;
+	if(k_right)  droll+=0.002;
+	if(k_tleft)  dyaw-=0.002;
+	if(k_tright) dyaw+=0.002;
+    */
+	if(keystate & K_UP)	     dpitch-=ROT_ADJ;
+	if(keystate & K_DOWN)	 dpitch+=ROT_ADJ;
+	if(keystate & K_LEFT)	 droll-=ROT_ADJ;
+	if(keystate & K_RIGHT)   droll+=ROT_ADJ;
+	if(keystate & K_TLEFT)   dyaw-=ROT_ADJ;
+	if(keystate & K_TRIGHT)  dyaw+=ROT_ADJ;
+
+	rot.yaw(dyaw);
+	rot.roll(droll);
+	rot.pitch(dpitch);
+	dyaw *= 0.95;
+	dpitch *= 0.95;
+	droll *= 0.95;
+    /*
+	vx += double(k_forth)*rot.getIndex(0,0)*0.005;
+	vy += double(k_forth)*rot.getIndex(1,0)*0.005;
+	vz += double(k_forth)*rot.getIndex(2,0)*0.005;
+	*/
+    double forth = (double)!!(keystate & K_FORTH);
+    vx += forth*rot.getIndex(0,0)*VEL_ADJ;
+	vy += forth*rot.getIndex(1,0)*VEL_ADJ;
+	vz += forth*rot.getIndex(2,0)*VEL_ADJ;
+	
+    /*
+    if(k_brakedebug)
+		vx=vy=vz=0.0;
+	if(k_brake) {
+		vx*=0.95;
+		vy*=0.95;
+		vz*=0.95;
+	}
+    */
+
+    if(keystate & K_BRAKEDEBUG){
+        vx = vy = vz = 0.0;
+    }
+    if(keystate & K_BRAKE){
+		vx*=0.95;
+		vy*=0.95;
+		vz*=0.95;
+    }
+
+	x+=vx;
+	y+=vy;
+	z+=vz;
+}
+void Player::procState(int key, bool state) {
+	switch( key) {
+		/*
+		   case SDLK_UP: pl.k_up = state;	break;
+		   case SDLK_DOWN: pl.k_down = state;	break;
+		   case SDLK_LEFT: pl.k_left = state;	break;
+		   case SDLK_RIGHT: pl.k_right = state;break;
+		   case SDLK_w: pl.k_forth = state; break;
+		   case SDLK_b: pl.k_brakedebug = state; break;
+		   case SDLK_s: pl.k_brake = state; break;
+		   case SDLK_a: pl.k_tleft = state; break;
+		   case SDLK_d: pl.k_tright = state; break;
+		 */
+
+		case SDLK_UP:    set_state(&keystate, K_UP, state); break;
+		case SDLK_DOWN:  set_state(&keystate, K_DOWN, state); break;
+		case SDLK_LEFT:  set_state(&keystate, K_LEFT, state); break;
+		case SDLK_RIGHT: set_state(&keystate, K_RIGHT, state); break;
+		case SDLK_w:     set_state(&keystate, K_FORTH, state); break;
+		case SDLK_b:     set_state(&keystate, K_BRAKEDEBUG, state); break;
+		case SDLK_s:     set_state(&keystate, K_BRAKE, state); break;
+		case SDLK_a:     set_state(&keystate, K_TLEFT, state); break;
+		case SDLK_d:     set_state(&keystate, K_TRIGHT, state); break;
+
+		
+	}
+}
+
