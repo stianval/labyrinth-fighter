341efff3736b8d25eee0cbb80af07c705c7861ca
thekian <thekian@ryuu>
2010-05-06 20:06:06 +0200

    Created maze class, moved code around.

--- src/cube.h
+++ src/cube.h
@@ -1,12 +1,3 @@
-#ifndef CUBE_H
-#define CUBE_H
-
-#define MAX_X 16
-#define MAX_Y 16
-#define MAX_Z 16
-#define MAP_SIZE (MAX_X*MAX_Y*MAX_Z)
-struct Cube {
-	int x;
-};
-
-#endif
+// Cube
+// Hypercube
+// Cube: Zero
