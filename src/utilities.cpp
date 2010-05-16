c495f45b3d8793506ed371464b905bb8ab10f7d8
stian <stian@localhost>
2010-05-16 23:22:34 +0200

    Draws a smiley

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -1,19 +1,45 @@
 #include "port.h"
 #include "utilities.h"
+#include "debug.h"
 TTF_Font *fontToDraw;
 
 void drawtext(int x, int y, int color, char text[]) 
 {
-  SDL_Color tmpfontcolor = {0,255,255,0};
+  SDL_Color tmpfontcolor = {255,255,255,255};
   //SDL_Color tmpfontbgcolor = {0, 0, 0, 0};
-  SDL_Surface *resulting_text;
+  SDL_Surface *message;
  
-  SDL_Rect dest = {x, y,255, 255};
+  //SDL_Rect dest = {x, y,255, 255};
 
-  resulting_text = TTF_RenderText_Solid(fontToDraw, text, tmpfontcolor);
-  SDL_BlitSurface(resulting_text, NULL, SDL_GetVideoSurface(), &dest);
+  message = TTF_RenderText_Solid(fontToDraw, text, tmpfontcolor);
+  //SDL_BlitSurface(resulting_text, NULL, SDL_GetVideoSurface(), &dest);
 
-  SDL_FreeSurface(resulting_text);
+
+  //Following will (probably) work with gl version 1.4 or later 
+  //glWindowPos2i(0,0);
+
+  int smiley[] = {
+      0x00000000, 0xCFFFFFFF, 0xCFFFFFFF, 0xCFFFFFFF, 0xCFFFFFFF, 0xCFFFFFFF, 0xCFFFFFFF, 0x00000000,
+      0xCFFFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0xCFFFFFFF,
+      0xCFFFFFFF, 0x00000000, 0x00000000, 0xCFFFFFFF, 0xCFFFFFFF, 0x00000000, 0x00000000, 0xCFFFFFFF,
+      0xCFFFFFFF, 0x00000000, 0xCFFFFFFF, 0x00000000, 0x00000000, 0xCFFFFFFF, 0x00000000, 0xCFFFFFFF,
+      0xCFFFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0xCFFFFFFF,
+      0xCFFFFFFF, 0x00000000, 0xCFFFFFFF, 0x00000000, 0x00000000, 0xCFFFFFFF, 0x00000000, 0xCFFFFFFF,
+      0xCFFFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0xCFFFFFFF,
+      0x00000000, 0xCFFFFFFF, 0xCFFFFFFF, 0xCFFFFFFF, 0xCFFFFFFF, 0xCFFFFFFF, 0xCFFFFFFF, 0x00000000
+  };
+
+  glPixelZoom(10,10);
+
+  // Displays the smiley.
+  glDrawPixels(8, 8, GL_RGBA, GL_UNSIGNED_BYTE, (void*)smiley);
+
+  // Wont work. It's slow and will only display a black rectangle
+  //glDrawPixels(message->w, message->h, GL_RED, GL_BYTE, message->pixels);
+
+  //PRINTV(glGetError());
+
+  SDL_FreeSurface(message);
   //resulting_text = TTF_RenderText_Shaded(fonttodraw, text, tmpfontcolor, tmpfontbgcolor);
   //resulting_text = TTF_RenderText_Blended(fonttodraw, text, tmpfontcolor);
 }
